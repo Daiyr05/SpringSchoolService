6bb2db13f609319fb29cc51754890c5b3978f417
Daiyr05 <87783710165q@gmail.com>
2024-05-02 13:57:18 +0500

    first commit

--- src/main/java/com/example/freecode/School.java
+++ src/main/java/com/example/freecode/School.java
@@ -1,4 +1,36 @@
 package com.example.freecode;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table
 public class School {
+    @Id
+    @SequenceGenerator(name = "school_sequence", sequenceName = "school_sequence", allocationSize = 1)
+    @GeneratedValue(generator = "school_sequence", strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+
+    public School() {
+    }
+
+    public School(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
